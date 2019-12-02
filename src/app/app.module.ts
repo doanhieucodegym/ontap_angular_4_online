0fe7eaf2145b57d494d0861a5658e2197327b7e9
DoanHieu <doanhieu.bl.hn@gmail.com>
2019-12-02 13:20:26 +0700

    service lay du lieu  hien thi commponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import { MoviesComponent } from './movies/movies.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import {MovieService} from './movie.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesComponent,
+    MovieDetailsComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    MovieService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Movie} from '../models/movie';
-import {fakeMovies} from '../fake-movies';
+// ko  laay truc tiep qua  day  ma  lay tu service
+// import {fakeMovies} from '../fake-movies';
+import {MovieService} from '../movie.service';
 
 @Component({
   selector: 'app-movies',
@@ -8,14 +10,26 @@ import {fakeMovies} from '../fake-movies';
   styleUrls: ['./movies.component.css']
 })
 export class MoviesComponent implements OnInit {
-  constructor() { }
+
   // khai bao ten  bien movie kieu  movie
   movie: Movie = {
     id: 1,
     name: 'Star Wars',
     releaseYear: 1997
   }
-  movies = fakeMovies;
+  // movies = fakeMovies;
+  // ko  lay truc  tiep ma  lay qua  service
+  movies: Movie[];
+  constructor(private movieService: MovieService) { }
+  // viet  ham lay du lieu  do vao movieService
+  getMoviesFormServices(): void {
+    this.movies = this.movieService.getMovies();
+  }
+  ngOnInit() {
+    this.getMoviesFormServices();
+  }
+
+
    // dinh nghia  ham  khi  click vao
   selectedMovie: Movie;
   onSelect(movie: Movie): void {
@@ -23,7 +37,6 @@ export class MoviesComponent implements OnInit {
     console.log(`selectedMovie = ${JSON.stringify(this.selectedMovie)}`);
   }
 
-  ngOnInit() {
-  }
+
 
 }
